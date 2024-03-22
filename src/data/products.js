309d030459da0737968d215e2b9c37a10bd8fb40
Wanderley Cabral da Silva <39313251+cabraldasilvac@users.noreply.github.com>
2024-03-22 14:45:55 -0300

    :rocket: feat: add data

---
+++ src/data/products.js
@@ -0,0 +1,24 @@
+// products.js
+
+const products = [
+    {
+        id: 1,
+        name: 'Produto 01',
+        description: 'Descrição do Produto 01',
+        price: 50.1,
+    },
+    {
+        id: 2,
+        name: 'Produto 02',
+        description: 'Descrição do Produto 02',
+        price: 75.1,
+    },
+    {
+        id: 3,
+        name: 'Produto 03',
+        description: 'Descrição do Produto 03',
+        price: 100.1,
+    },
+]
+
+export default products
---
+++ src/data/services.js
@@ -0,0 +1,23 @@
+//services.js
+const services = [
+    {
+        id: 1,
+        name: 'Serviço 1',
+        description: 'Descrição do Serviço 1',
+        price: 80.2,
+    },
+    {
+        id: 2,
+        name: 'Serviço 2',
+        description: 'Descrição do Serviço 2',
+        price: 120.2,
+    },
+    {
+        id: 3,
+        name: 'Serviço 3',
+        description: 'Descrição do Serviço 3',
+        price: 151.2,
+    },
+]
+
+export default services
